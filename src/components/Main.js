dd477c29333dc6160e2c6e6f985b4676ef5993a8
tianyaxiang <tianyaxiang@qq.com>
2016-06-17 18:24:39 +0800

    test test

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,12 +5,27 @@ import React from 'react';
 
 let yeomanImage = require('../images/yeoman.png');
 
-class AppComponent extends React.Component {
+class Test extends React.Component {
   render() {
     return (
       <div className="index">
         <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+        <div className="notice">intpubt to get started!</div>
+      </div>
+    );
+  }
+}
+
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <div>
+      <Test></Test>
+      <div className="index">
+        <img src={yeomanImage} alt="Yeoman Generator" />
+        <div className="notice">Qt  edit <code>src/components/Main.js</code> to get started!</div>
+      </div>
       </div>
     );
   }
